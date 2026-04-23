1fad09e823cf5d420fc5069500329994da899889
Deepika Gauba <deepika@Deepikas-MacBook-Pro-2.local>
2026-04-23 14:10:38 +0530

    Initial commit

---
+++ src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
@@ -0,0 +1,43 @@
+package QuantityMeasurementApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MeasurementApplicationTests {
+
+    @Test
+	void testFeetEquality_SameValue(){
+		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+		MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(89.6);
+
+		assertEquals(f1, f2);
+	}
+	@Test
+	void testFeetEquality_DifferValue(){
+		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+		MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(34.6); 
+		
+		assertNotEquals(f1,f2);
+	}
+	@Test
+	void testFeetNullable_NullValue(){
+		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(55.0);
+		
+		assertFalse(f1.equals(null));
+	}
+	@Test
+	void testFeetEquality_ClassComparison(){
+		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+		
+		assertFalse(f1.equals("Some String"));
+	}
+	@Test
+	void testFeetEquality_SameReference(){
+		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
+		
+		assertTrue(f1.equals(f1));
+	}
+
+
+}
